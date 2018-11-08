837f2eacc34f15f98493aa20702f9730675a6f82
FilipSwiatek <44712190+FilipSwiatek@users.noreply.github.com>
2018-11-08 13:42:55 +0100

    Update SLCD.h

--- SLCD.h
+++ SLCD.h
@@ -120,7 +120,11 @@ uint8_t LCD_DotClear(uint8_t dot)
 
 void LCD_Clear(void)
 {
-	for(int i=0;i<15;i++) LCD->WF [i]=0U; // clearing all WF registers
+	for(int i=0;i<4;i++)  // clearing all WF registers
+	{
+		LCD->WF8B[digit_ABC[i]]=0x0;
+		LCD->WF8B[digit_DEFG[i]]=0x0;
+	}
 }
 
 uint8_t LCD_Dec_Print(uint16_t value)
